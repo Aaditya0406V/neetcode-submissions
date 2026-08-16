7a1cb23b186477725667dfa5cab2e758f0b36efd
Aaditya0406V <aadityavidhale04@gmail.com>
2026-08-16 22:26:45 +0530

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0;i<nums.length;i++)
+        {
+            set.add(nums[i]);
+            if(set.contains(target))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
