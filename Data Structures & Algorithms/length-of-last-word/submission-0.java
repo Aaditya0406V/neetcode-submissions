92a4acea013f9d2dbb7f2ec8b925e6656158670b
Aaditya0406V <aadityavidhale04@gmail.com>
2026-08-23 01:41:30 +0530

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String arr[] = s.split(" ");
+        int length = arr[arr.length-1].length();
+        return length;
+    }
+}
